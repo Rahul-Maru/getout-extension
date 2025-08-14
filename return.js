b27dc9ad309c1718ac6db4d8479f41888f1111d0
Rahul Maru <rahulmaru3507@gmail.com>
2025-08-14 15:32:02 +0530

    add styling, radio box

--- return.js
+++ return.js
@@ -1,11 +1,23 @@
-function goback() {
+const sleep = s => new Promise(r => setTimeout(r, 1000*s));
+
+async function goback() {
+	const timer = document.getElementById("timer")
 	const form = document.getElementById("return")
-	
-	const url = new URL(document.URL)
-	dest = url.searchParams.get("src")
-
+	const button = document.getElementsByTagName("button")[0]
 	const p = document.getElementById("test")
-	p.textContent = dest
 
+	const url = new URL(document.URL)
+	const dest = url.searchParams.get("src") || "nowhere...?"
+	p.textContent = "came from: " + dest
+
+	let t = Number(timer.textContent.slice(0, -1))
+	while (t > 0) {
+		await sleep(1)
+		t--
+		timer.textContent = t + "s"
+	}
+
+	button.setAttribute("style", "background-color: green;")
 	form.setAttribute("action", dest)
+	button.type = "submit"
 }
